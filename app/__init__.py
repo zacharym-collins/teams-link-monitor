d71ee2eb029a2b594e9ffb6242893893d0555aea
Zachary Collins <colz5081@gmail.com>
2026-05-28 11:43:08 -0500

    ENH: Add modules to __all__
    TeamsNotifier and capture_desktop_state are configured so that they can be imported directly from aghes_teams_monitor

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,6 @@
 """An application that sends logging messages to a designated MS Teams Channel."""
+from app.notifier import TeamsNotifier
+from app.screenshot import capture_desktop_state
 
+__all__ = ["TeamsNotifier", "capture_desktop_state"]
 __version__ = "0.0.a1"
